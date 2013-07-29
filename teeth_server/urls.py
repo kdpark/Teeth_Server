d366be15070d38a857900e028f6bc3092814a2ef
Kyle Park <wha754@gmail.com>
2013-07-29 15:17:20 -0400

    books examples and some admin

--- teeth_server/urls.py
+++ teeth_server/urls.py
@@ -1,12 +1,15 @@
 from django.conf.urls import patterns, include, url
-
+from teeth_server.views import home, current_datetime, hour_ahead
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
 	url(r'^$', 'teeth_server.views.home', name='home'),
+	url(r'^hello/$', home),
+	url(r'^time/$', current_datetime),
+	url(r'^time/plus/(\d+)/$', hour_ahead),
     # url(r'^teeth_server/', include('teeth_server.foo.urls')),
 	
 	# url(r'^invite/$', views.invite, name='invite'),
@@ -15,5 +18,5 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 )
