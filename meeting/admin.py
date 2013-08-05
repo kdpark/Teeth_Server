3d6f68e13c543761372a1396a36cac06784779b6
Kyle Park <wha754@gmail.com>
2013-08-05 10:57:06 -0400

    add signin module

--- meeting/admin.py
+++ meeting/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin  
+from meeting.models import User
 
+#class UserAdmin(admin.ModelAdmin):
+	#fields = ('name', 'password', 'email')
+
+admin.site.register(User)
 
 """
 from books.models import Publisher, Author, Book  
